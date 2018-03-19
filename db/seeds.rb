4f3022117039650b307589c957dcb0b68b2cd6c9
Arturo Duarte <arturoduarte@users.noreply.github.com>
2018-03-19 13:23:42 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,4 +33,4 @@ end
 
 
 
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
